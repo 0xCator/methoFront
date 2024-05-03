f2653ee28e29fd280ff7c622b9893018fff4faac
SIGMazer <mazinasd7@gmail.com>
2024-05-03 16:57:47 +0300

    feat: custom image view
    Signed-off-by: SIGMazer <mazinasd7@gmail.com>

--- src/Components/MainPage/ImageView/ImageView.tsx
+++ src/Components/MainPage/ImageView/ImageView.tsx
@@ -4,11 +4,10 @@ import styles from './ImageView.module.css';
 const ImageView: React.FC<ImageFile> = (props) => {
     return <div className="cardDesign">
         <div className="card">
-            <img className={`card-img-top ${styles.imageSize}`}  src={props.filePath} alt="" />
-            <div className="card-body">
-                <h4 className={`card-title ${styles.titleSize}`}>{props.fileName}</h4>
-                <h5 className={`card-title ${styles.textSize}`}>{"by "+ props.userName}</h5>
-            </div>
+        <img title={props.fileName} className={`card-img-top ${styles.imageSize}`}  src={props.filePath} alt="" />
+        <div className={`card-body ${styles.imageText}`}>
+        <h5 className={`card-title ${styles.textSize}`}>{"by "+ props.userName}</h5>
+        </div>
         </div>
     </div>
 }
